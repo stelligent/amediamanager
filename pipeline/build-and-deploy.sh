880134c445797ceabbfa945690c9703088acadc8
Christopher Petro <cpetro1@gmail.com>
2014-07-09 12:53:41 -0400

    Update build-and-deploy.sh

--- pipeline/build-and-deploy.sh
+++ pipeline/build-and-deploy.sh
@@ -10,7 +10,7 @@ gem install trollop opendelivery --no-ri --no-rdoc
 gem install aws-sdk-core --pre --no-ri --no-rdoc
 export stack_name=AMM_App-$timestamp
 ruby -e 'require "opendelivery"' -e "OpenDelivery::Domain.new('$region').set_property '$sdb_domain','$pipeline_instance_id', 'stack_name', '$stack_name'"
-aws cloudformation create-stack  --stack-name $stack_name  --template-body file://cfn/vpc/amm-master.cfn.json --region ${region}  --disable-rollback  --capabilities="CAPABILITY_IAM" \
+aws cloudformation create-stack  --stack-name $stack_name  --template-body file://cfn/vpc/amm-master.cfn.json  --parameters file://cfn/vpc/launch-params.json --region ${region}  --disable-rollback  --capabilities="CAPABILITY_IAM"
 
 # make sure we give AWS a chance to actually create the stack...
 sleep 30
