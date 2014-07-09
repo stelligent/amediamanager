977f5376c3795da76a6a34be043dbf9b9de4c6c1
Chris <cpetro1@gmail.com>
2014-07-09 12:14:28 -0400

    added params keys...test

--- pipeline/build-and-deploy.sh
+++ pipeline/build-and-deploy.sh
@@ -10,7 +10,13 @@ gem install trollop opendelivery --no-ri --no-rdoc
 gem install aws-sdk-core --pre --no-ri --no-rdoc
 export stack_name=AMM_App-$timestamp
 ruby -e 'require "opendelivery"' -e "OpenDelivery::Domain.new('$region').set_property '$sdb_domain','$pipeline_instance_id', 'stack_name', '$stack_name'"
-aws cloudformation create-stack  --stack-name $stack_name  --template-body file://cfn/vpc/amm-master.cfn.json  --parameters file://cfn/vpc/launch-params.json --region ${region}  --disable-rollback  --capabilities="CAPABILITY_IAM"
+aws cloudformation create-stack  --stack-name $stack_name  --template-body file://cfn/vpc/amm-master.cfn.json  --parameters file://cfn/vpc/launch-params.json --region ${region}  --disable-rollback  --capabilities="CAPABILITY_IAM" \
+--parameters \
+  ParameterKey=vpc,ParameterValue=$vpc \
+  ParameterKey=publicSubnet,ParameterValue=$publicSubnet \
+  ParameterKey=privateSubnetA,ParameterValue=$privateSubnetA \
+  ParameterKey=privateSubnetB,ParameterValue=$privateSubnetB
+
 # make sure we give AWS a chance to actually create the stack...
 sleep 30
 ruby pipeline/bin/monitor_stack.rb  --stack $stack_name --region ${region}
